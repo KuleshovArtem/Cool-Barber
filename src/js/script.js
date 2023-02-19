51ef37a766802d9eefd295779826ff2b5322c547
artem kuleshov <kooleshov.artem@gmail.com>
2023-02-19 10:13:47 +0500

    реализован слайдер , но убрана подгрузка слайдом из базы

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,68 @@
-// генерирует верстку . отзывы
-function createTestimonials(data) {
-    data.forEach(({img, alt, descr, text}) => {
-        const element = document.createElement('div');
-        element.classList.add("testimonials__item");
-        element.innerHTML = `
-        <div class="testimonials__img">
-            <img src=${img} alt=${alt}>
-        </div>
-        <div class="testimonials__descr">${descr}</div>
-        <div class="testimonials__text">${text}</div>
-        <a class="testimonials__link">Read full testimonial</a>
-        `;
-        document.querySelector(".testimonials__wrapper").append(element);
-    });
+window.addEventListener('DOMContentLoaded', () => {
+
+// // функция получения данных с сервера 
+// const getData = async (url) => {
+//     const res = await fetch(url);
+//     return res.json();
+// };
+
+// // генерирует верстку . отзывы
+// function createTestimonials(data) {
+//     data.forEach(({img, alt, descr, text}) => {
+//         const element = document.createElement('div');
+//         element.classList.add("testimonials__item");
+//         element.innerHTML = `
+//         <div class="testimonials__img">
+//             <img src=${img} alt=${alt}>
+//         </div>
+//         <div class="testimonials__descr">${descr}</div>
+//         <div class="testimonials__text">${text}</div>
+//         <a class="testimonials__link">Read full testimonial</a>
+//         `;
+//         document.querySelector(".testimonials__inner").append(element); 
+//     });
+// }
+
+// getData('http://localhost:3000/menu')
+//     .then(data => createTestimonials(data));
+});
+
+const slides = document.querySelectorAll('.testimonials__item'),
+//slider = document.querySelector('.testimonials'),
+//slidesWrapper = document.querySelector('.testimonials__wrapper'),
+slidesField = document.querySelector('.testimonials__inner'),
+btnNext = document.querySelector('.testimonials__slider-next'),
+btnPrev = document.querySelector('.testimonials__slider-prev'),
+//width = window.getComputedStyle(slidesWrapper).width,
+widthSlide = window.getComputedStyle(slides[2]).width;
+
+let offset = 0;
+
+//удаление у строки букв
+function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
 }
-// функция получения данных с сервера 
-const getData = async (url) => {
-    const res = await fetch(url);
-    console.log(res);
-
-    return res.json();
-};
-
-getData('http://localhost:3000/menu')
-    .then(data => createTestimonials(data));
-
+//;
+btnNext.addEventListener('click', () => {
     
+    if(offset == (deleteNotDigits(widthSlide) + 20) * (slides.length - 4)) {
+        offset = 0;
+    } else {
+        offset += deleteNotDigits(widthSlide) + 20;
+    }
+    console.log(offset);
+    slidesField.style.transform = `translateX(-${offset}px)`;
+});
+
+btnPrev.addEventListener('click', () => {
+    console.log(slides.length);
+    if(offset == 0) {
+        offset = (deleteNotDigits(widthSlide) + 20) * (slides.length - 4);
+    } else {   
+        offset -= deleteNotDigits(widthSlide) + 20;
+    }
+    console.log(offset);
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+});
