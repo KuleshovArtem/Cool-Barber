6e771d2722fc3b800ffed3754b46d8939c779a79
artem kuleshov <kooleshov.artem@gmail.com>
2023-02-20 15:04:50 +0500

    реализация динамического отображения сеции team

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,9 @@ window.addEventListener('DOMContentLoaded', () => {
         blockPrice = document.querySelector('.price'),
         blockConsultation = document.querySelector('.consultation'),
         blockContacts = document.querySelector('.contacts'),
-        btnContact = document.querySelector('[data-contacts]');
+        btnContact = document.querySelector('[data-contacts]'),
+        blockTeam = document.querySelector('.team'),
+        btnTeam = document.querySelectorAll('[data-team]');
 
 
     function hideBlock() {
@@ -25,12 +27,24 @@ window.addEventListener('DOMContentLoaded', () => {
             blockAbout.classList.remove('hide');
             blockTestimonials.classList.remove('hide');
             blockPrice.classList.remove('hide');
+            blockTeam.classList.add('hide');
         }
         
     }
-    function showBlockTeam () {
+    
+    btnLogo.addEventListener('click', () => showBlock());
 
+    // show form
+    function  showContacts () {
+        const formConsultation = document.querySelector('.consultation__wrapper');
+
+        blockContacts.style.marginTop= '0px';
+        blockContacts.style.height = '1100px';
+        formConsultation.classList.add('consultation__wrapper-bottom');
+        hideBlock();
     }
+    
+    btnContact.addEventListener('click', () => showContacts());
 
     // функция получения данных с сервера 
     const getData = async (url) => {
@@ -54,11 +68,60 @@ window.addEventListener('DOMContentLoaded', () => {
             document.querySelector(".testimonials__inner").append(element); 
         });
     }
+   
+    function createTeam(data) {
+        data.forEach(({avatar, name, descrMaster, descrStyle, work1, work2, work3, work4, work5, work6, work7, work8 }) => {
+            const element = document.createElement('div');
+            element.classList.add('team__item');
+            element.innerHTML = `
+            <div class="team__header">
+                <div class="team__avatar">
+                    <img src=${avatar} alt="avatar">
+                </div>
+                <div class="team__name">${name}</div>
+                <div class="team__descr">${descrMaster}</div>
+                <div class="team__descr">${descrStyle}</div>
+            </div>
+            <div class="team__gallery">
+                <div class="team__gallery-item"><img src=${work1} alt="work"></div>
+                <div class="team__gallery-item"><img src=${work2} alt="work"></div>
+                <div class="team__gallery-item"><img src=${work3} alt="work"></div>
+                <div class="team__gallery-item"><img src=${work4} alt="work"></div>
+                <div class="team__gallery-item"><img src=${work5} alt="work"></div>
+                <div class="team__gallery-item"><img src=${work6} alt="work"></div>
+                <div class="team__gallery-item"><img src=${work7} alt="work"></div>
+                <div class="team__gallery-item"><img src=${work8} alt="work"></div>
+            </div>
+            <button class="button button-team">See all works</button>
+            `;
+            document.querySelector('.team__masters').append(element);
+        });
+    }
 
     getData('http://localhost:3000/testimonials')
-    .then(data => createTestimonials(data))
-    .catch(() => console.error('error'))  // написать функционал вывода на страницу ошибки
-    .then(() => slider());
+        .then(data => createTestimonials(data))
+        .catch(() => console.error('error'))  // написать функционал вывода на страницу ошибки
+        .then(() => slider());
+
+
+    getData('http://localhost:3000/team')
+        .then(data => createTeam(data))
+        .catch(() => console.error('error'));  // написать функционал вывода на страницу ошибки
+
+    function showBlockTeam () {
+        getData('http://localhost:3000/team')
+            .then(()=> {
+                //hideBlock();
+                //blockTeam.classList.remove('hide');
+               // window.scrollTo(0, 0);
+            })
+            //.then(data => createTeam(data))
+            .catch(() => console.error('error'));  // написать функционал вывода на страницу ошибки
+    }
+
+    btnTeam.forEach(i => {
+        i.addEventListener('click', () => showBlockTeam());
+    });
 
 
 //slider
@@ -94,19 +157,6 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // show form
-
-    function  showContacts () {
-        const formConsultation = document.querySelector('.consultation__wrapper');
-
-        blockContacts.style.marginTop= '0px';
-        blockContacts.style.height = '1100px';
-        formConsultation.classList.add('consultation__wrapper-bottom');
-        hideBlock();
-    }
-    
-    btnContact.addEventListener('click', () => showContacts());
-    btnLogo.addEventListener('click', () => showBlock());
     
    
 
